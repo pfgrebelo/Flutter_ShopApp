c10440844dc4ed74d0ac1d7360efa1032b682e7e
Pedro Rebelo <pfiliperebelo@gmail.com>
2022-12-19 10:58:08 +0000

    expanded order list

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,13 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,15 +23,42 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('€${order.amount}'),
+            title: Text('€${widget.order.amount.toStringAsFixed(2)}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              height: min(widget.order.products.length * 20 + 10, 100),
+              child: ListView(
+                  children: widget.order.products
+                      .map(
+                        (prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              prod.title,
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            Text(
+                              '${prod.quantity}x €${prod.price}',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      )
+                      .toList()),
+            )
         ],
       ),
     );
