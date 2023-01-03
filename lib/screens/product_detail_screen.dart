59163e7a3e0a59ce8e1eac7b2400f19938c8c286
Pedro Rebelo <pfiliperebelo@gmail.com>
2023-01-03 12:17:03 +0000

    added CustomScrollView, slivers, gradient, stroke

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -20,43 +20,79 @@ class ProductDetailScreen extends StatelessWidget {
       listen: false, //DO NOT REBUILD ON CHANGES, DEACTIVATE LISTENER
     ).findById(productId); //GETS THE SPECIFIC PRODUCT
     return Scaffold(
-      appBar: AppBar(
+      /* appBar: AppBar(
         title: Text(loadedProduct.title),
-      ),
+      ), */
       body: Scrollbar(
         thumbVisibility: true,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Hero(                //LINKED ANIMATED IMAGE FROM PRODUCT ITEM(ProductsOverviewScreen)
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 300,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Stack(children: [  //STACK FOR TEXT STROKE, 1st THE STROKE, 2nd THE FILL
+                  Text(
+                    loadedProduct.title,
+                    style: TextStyle(
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 2
+                        ..color = Colors.black,
+                    ),
+                  ),
+                  Text(
+                    loadedProduct.title,
+                    style: TextStyle(
+                      foreground: Paint()     //FILLED TEXT WITH GRADIENT
+                        ..shader = LinearGradient(
+                          colors: <Color>[
+                            Colors.indigo,
+                            Colors.white,
+                            Colors.blue
+                            //add more color here.
+                          ],
+                        ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 100.0))
+                        ..style,
+                    ),
+                    //
+                  ),
+                ]),
+                background: Hero(
+                  //LINKED ANIMATED IMAGE FROM PRODUCT ITEM(ProductsOverviewScreen)
                   tag: loadedProduct.id,
                   child: Image.network(
                     loadedProduct.imageUrl,
-                    fit: BoxFit.contain,
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              SizedBox(height: 10),
-              Text(
-                '€${loadedProduct.price}',
-                style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  SizedBox(height: 10),
+                  Text(
+                    '€${loadedProduct.price}',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    width: double.infinity,
+                    child: Text(
+                      loadedProduct.description,
+                      style: Theme.of(context).textTheme.titleSmall,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                    ),
+                  ),
+                  SizedBox(height: 800),
+                ],
               ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                width: double.infinity,
-                child: Text(
-                  loadedProduct.description,
-                  style: Theme.of(context).textTheme.titleSmall,
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
